87d91f41d4b2b9811bf90e3f9c7f079a1e93a0d7
Lexor <lesha.goloborodko392@gmail.com>
2023-09-02 18:30:02 +0300

    8. Оптимизирован скрипт

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -25,35 +25,55 @@ $(document).ready(function () {
                 $(".circle").removeClass("active");
                 $(".circle").css("transform", "rotate(5076deg) scale(1)");
             }, 15000);
+            // setTimeout(function () {
+            //     $(".circle").css("transform", "rotate(5076deg) scale(1.02)");
+            // }, 15200);
         }
     });
 
     function applyParallaxEffect(element, intensity) {
+        var rAF;
+        var currentTransform = 'translate3d(0, 0, 0)';
+        var isMoving = false;
+
         $(document).mousemove(function(e) {
             if (element.length === 0) return; // Проверка на существование элемента
 
-            var mouseX = e.pageX;
-            var mouseY = e.pageY;
+            if (!isMoving) {
+                isMoving = true;
 
-            var centerX = element.offset().left + element.width() / 2;
-            var centerY = element.offset().top + element.height() / 2;
+                var mouseX = e.pageX;
+                var mouseY = e.pageY;
 
-            var deltaX = mouseX - centerX;
-            var deltaY = mouseY - centerY;
+                var centerX = element.offset().left + element.width() / 2;
+                var centerY = element.offset().top + element.height() / 2;
 
-            var percentX = deltaX / (element.width() / 2);
-            var percentY = deltaY / (element.height() / 2);
+                var deltaX = mouseX - centerX;
+                var deltaY = mouseY - centerY;
 
-            var parallaxX = percentX * intensity;
-            var parallaxY = percentY * intensity;
+                var percentX = deltaX / (element.width() / 2);
+                var percentY = deltaY / (element.height() / 2);
 
-            // Используем transform для плавного движения
-            element.css({
-                'transform': 'translate3d(' + parallaxX + 'px,' + parallaxY + 'px, 0)'
-            });
+                var parallaxX = percentX * intensity;
+                var parallaxY = percentY * intensity;
+
+                var newTransform = 'translate3d(' + parallaxX + 'px,' + parallaxY + 'px, 0)';
+
+                if (newTransform !== currentTransform) {
+                    currentTransform = newTransform;
+
+                    cancelAnimationFrame(rAF);
+                    rAF = requestAnimationFrame(function() {
+                        // Используем transform для плавного движения
+                        element.css({
+                            'transform': newTransform
+                        });
+                        isMoving = false;
+                    });
+                }
+            }
         });
     }
-
     // Применить эффект к .circle с интенсивностью 20
     applyParallaxEffect($('.moove20'), 20);
 
